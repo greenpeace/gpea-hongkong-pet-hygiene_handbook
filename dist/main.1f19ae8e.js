c23f8507973caf469fb3978da5f94dc8289134cc
Chris Chan <eahk@users.noreply.github.com>
2020-02-24 13:08:08 +0800

    adapt the new en uploader

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -195,7 +195,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../img/ill/index_bg.svg":[["index_bg.c1b192b4.svg","assets/img/ill/index_bg.svg"],"assets/img/ill/index_bg.svg"],"_css_loader":"../node_modules/parcel/src/builtins/css-loader.js"}],"main.scss":[function(require,module,exports) {
+},{"_css_loader":"../node_modules/parcel/src/builtins/css-loader.js"}],"main.scss":[function(require,module,exports) {
 var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
@@ -11012,7 +11012,12 @@ if ( !noGlobal ) {
 return jQuery;
 } );
 
-},{"process":"../node_modules/process/browser.js"}],"../node_modules/mailcheck/src/mailcheck.js":[function(require,module,exports) {
+},{"process":"../node_modules/process/browser.js"}],"../node_modules/smooth-scroll/dist/smooth-scroll.polyfills.min.js":[function(require,module,exports) {
+var define;
+var global = arguments[3];
+/*! smooth-scroll v16.1.2 | (c) 2020 Chris Ferdinandi | MIT License | http://github.com/cferdinandi/smooth-scroll */
+window.Element&&!Element.prototype.closest&&(Element.prototype.closest=function(e){var t,n=(this.document||this.ownerDocument).querySelectorAll(e),o=this;do{for(t=n.length;0<=--t&&n.item(t)!==o;);}while(t<0&&(o=o.parentElement));return o}),(function(){if("function"==typeof window.CustomEvent)return;function e(e,t){t=t||{bubbles:!1,cancelable:!1,detail:void 0};var n=document.createEvent("CustomEvent");return n.initCustomEvent(e,t.bubbles,t.cancelable,t.detail),n}e.prototype=window.Event.prototype,window.CustomEvent=e})(),(function(){for(var r=0,e=["ms","moz","webkit","o"],t=0;t<e.length&&!window.requestAnimationFrame;++t)window.requestAnimationFrame=window[e[t]+"RequestAnimationFrame"],window.cancelAnimationFrame=window[e[t]+"CancelAnimationFrame"]||window[e[t]+"CancelRequestAnimationFrame"];window.requestAnimationFrame||(window.requestAnimationFrame=function(e,t){var n=(new Date).getTime(),o=Math.max(0,16-(n-r)),a=window.setTimeout((function(){e(n+o)}),o);return r=n+o,a}),window.cancelAnimationFrame||(window.cancelAnimationFrame=function(e){clearTimeout(e)})})(),(function(e,t){"function"==typeof define&&define.amd?define([],(function(){return t(e)})):"object"==typeof exports?module.exports=t(e):e.SmoothScroll=t(e)})("undefined"!=typeof global?global:"undefined"!=typeof window?window:this,(function(q){"use strict";var I={ignore:"[data-scroll-ignore]",header:null,topOnEmptyHash:!0,speed:500,speedAsDuration:!1,durationMax:null,durationMin:null,clip:!0,offset:0,easing:"easeInOutCubic",customEasing:null,updateURL:!0,popstate:!0,emitEvents:!0},F=function(){var n={};return Array.prototype.forEach.call(arguments,(function(e){for(var t in e){if(!e.hasOwnProperty(t))return;n[t]=e[t]}})),n},r=function(e){"#"===e.charAt(0)&&(e=e.substr(1));for(var t,n=String(e),o=n.length,a=-1,r="",i=n.charCodeAt(0);++a<o;){if(0===(t=n.charCodeAt(a)))throw new InvalidCharacterError("Invalid character: the input contains U+0000.");1<=t&&t<=31||127==t||0===a&&48<=t&&t<=57||1===a&&48<=t&&t<=57&&45===i?r+="\\"+t.toString(16)+" ":r+=128<=t||45===t||95===t||48<=t&&t<=57||65<=t&&t<=90||97<=t&&t<=122?n.charAt(a):"\\"+n.charAt(a)}return"#"+r},L=function(){return Math.max(document.body.scrollHeight,document.documentElement.scrollHeight,document.body.offsetHeight,document.documentElement.offsetHeight,document.body.clientHeight,document.documentElement.clientHeight)},x=function(e){return e?(t=e,parseInt(q.getComputedStyle(t).height,10)+e.offsetTop):0;var t},H=function(e,t,n,o){if(t.emitEvents&&"function"==typeof q.CustomEvent){var a=new CustomEvent(e,{bubbles:!0,detail:{anchor:n,toggle:o}});document.dispatchEvent(a)}};return function(o,e){var A,a,O,C,M={};M.cancelScroll=function(e){cancelAnimationFrame(C),C=null,e||H("scrollCancel",A)},M.animateScroll=function(i,c,e){M.cancelScroll();var s=F(A||I,e||{}),u="[object Number]"===Object.prototype.toString.call(i),t=u||!i.tagName?null:i;if(u||t){var l=q.pageYOffset;s.header&&!O&&(O=document.querySelector(s.header));var n,o,a,m,r,d,f,h,p=x(O),g=u?i:(function(e,t,n,o){var a=0;if(e.offsetParent)for(;a+=e.offsetTop,e=e.offsetParent;);return a=Math.max(a-t-n,0),o&&(a=Math.min(a,L()-q.innerHeight)),a})(t,p,parseInt("function"==typeof s.offset?s.offset(i,c):s.offset,10),s.clip),y=g-l,v=L(),w=0,S=(n=y,a=(o=s).speedAsDuration?o.speed:Math.abs(n/1e3*o.speed),o.durationMax&&a>o.durationMax?o.durationMax:o.durationMin&&a<o.durationMin?o.durationMin:parseInt(a,10)),E=function(e,t){var n,o,a,r=q.pageYOffset;if(e==t||r==t||(l<t&&q.innerHeight+r)>=v)return M.cancelScroll(!0),o=t,a=u,0===(n=i)&&document.body.focus(),a||(n.focus(),document.activeElement!==n&&(n.setAttribute("tabindex","-1"),n.focus(),n.style.outline="none"),q.scrollTo(0,o)),H("scrollStop",s,i,c),!(C=m=null)},b=function(e){var t,n,o;m||(m=e),w+=e-m,d=l+y*(n=r=1<(r=0===S?0:w/S)?1:r,"easeInQuad"===(t=s).easing&&(o=n*n),"easeOutQuad"===t.easing&&(o=n*(2-n)),"easeInOutQuad"===t.easing&&(o=n<.5?2*n*n:(4-2*n)*n-1),"easeInCubic"===t.easing&&(o=n*n*n),"easeOutCubic"===t.easing&&(o=--n*n*n+1),"easeInOutCubic"===t.easing&&(o=n<.5?4*n*n*n:(n-1)*(2*n-2)*(2*n-2)+1),"easeInQuart"===t.easing&&(o=n*n*n*n),"easeOutQuart"===t.easing&&(o=1- --n*n*n*n),"easeInOutQuart"===t.easing&&(o=n<.5?8*n*n*n*n:1-8*--n*n*n*n),"easeInQuint"===t.easing&&(o=n*n*n*n*n),"easeOutQuint"===t.easing&&(o=1+--n*n*n*n*n),"easeInOutQuint"===t.easing&&(o=n<.5?16*n*n*n*n*n:1+16*--n*n*n*n*n),t.customEasing&&(o=t.customEasing(n)),o||n),q.scrollTo(0,Math.floor(d)),E(d,g)||(C=q.requestAnimationFrame(b),m=e)};0===q.pageYOffset&&q.scrollTo(0,0),f=i,h=s,u||history.pushState&&h.updateURL&&history.pushState({smoothScroll:JSON.stringify(h),anchor:f.id},document.title,f===document.documentElement?"#top":"#"+f.id),"matchMedia"in q&&q.matchMedia("(prefers-reduced-motion)").matches?q.scrollTo(0,Math.floor(g)):(H("scrollStart",s,i,c),M.cancelScroll(!0),q.requestAnimationFrame(b))}};var t=function(e){if(!e.defaultPrevented&&!(0!==e.button||e.metaKey||e.ctrlKey||e.shiftKey)&&"closest"in e.target&&(a=e.target.closest(o))&&"a"===a.tagName.toLowerCase()&&!e.target.closest(A.ignore)&&a.hostname===q.location.hostname&&a.pathname===q.location.pathname&&/#/.test(a.href)){var t,n;try{t=r(decodeURIComponent(a.hash))}catch(e){t=r(a.hash)}if("#"===t){if(!A.topOnEmptyHash)return;n=document.documentElement}else n=document.querySelector(t);(n=n||"#top"!==t?n:document.documentElement)&&(e.preventDefault(),(function(e){if(history.replaceState&&e.updateURL&&!history.state){var t=q.location.hash;t=t||"",history.replaceState({smoothScroll:JSON.stringify(e),anchor:t||q.pageYOffset},document.title,t||q.location.href)}})(A),M.animateScroll(n,a))}},n=function(e){if(null!==history.state&&history.state.smoothScroll&&history.state.smoothScroll===JSON.stringify(A)){var t=history.state.anchor;"string"==typeof t&&t&&!(t=document.querySelector(r(history.state.anchor)))||M.animateScroll(t,null,{updateURL:!1})}};M.destroy=function(){A&&(document.removeEventListener("click",t,!1),q.removeEventListener("popstate",n,!1),M.cancelScroll(),C=O=a=A=null)};return (function(){if(!("querySelector"in document&&"addEventListener"in q&&"requestAnimationFrame"in q&&"closest"in q.Element.prototype))throw"Smooth Scroll: This browser does not support the required JavaScript methods and browser APIs.";M.destroy(),A=F(I,e||{}),O=A.header?document.querySelector(A.header):null,document.addEventListener("click",t,!1),A.updateURL&&A.popstate&&q.addEventListener("popstate",n,!1)})(),M}}));
+},{}],"../node_modules/mailcheck/src/mailcheck.js":[function(require,module,exports) {
 var define;
 /*
  * Mailcheck https://github.com/mailcheck/mailcheck
@@ -21815,6 +21820,8 @@ require("./main.scss");
 
 var _jquery = _interopRequireDefault(require("jquery"));
 
+var _smoothScroll = _interopRequireDefault(require("smooth-scroll"));
+
 var _mailcheck = _interopRequireDefault(require("mailcheck"));
 
 var yup = _interopRequireWildcard(require("yup"));
@@ -21825,17 +21832,17 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-//
-//
 (0, _jquery.default)(function () {
-  var init = function init() {
+  var scroll = new _smoothScroll.default('a[href*="#"]'); //
+
+  var enformInit = function enformInit() {
     (0, _jquery.default)(".en__field--emailAddress").append("<span class=\"mailcheck-message\"></span>");
     (0, _jquery.default)(".en__field--lastName").hide();
     (0, _jquery.default)(".en__field--firstName").hide();
     (0, _jquery.default)(".en__submit button").addClass("btn btn-block btn-round btn--download");
   };
 
-  init(); //
+  enformInit(); //
 
   var email = document.querySelector('input[name="supporter.emailAddress"]');
   var mailcheckMessage = document.querySelector(".mailcheck-message");
@@ -21856,37 +21863,75 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
     });
   };
 
-  email.addEventListener("blur", mailcheck());
-  mailcheckMessage.addEventListener("click", function () {
-    var emailSuggestion = document.querySelector(".email-suggestion").innerText;
-
-    if (emailSuggestion) {
-      email.value = emailSuggestion;
-      this.innerText = "";
-    }
-  });
   var validationSchema = yup.object({
     email: yup.string().email().required()
-  }); //
+  });
 
-  var enformButton = document.querySelector(".en__submit");
-  enformButton.addEventListener("click", function (event) {
-    event.preventDefault();
-    var enform = document.querySelector("form.en__component");
-    var isValid = validationSchema.isValid({
-      email: email.value
-    });
-    isValid.then(function (valid) {
-      if (valid) {
-        mailcheckMessage.innerText = "";
-        enform.submit();
-      } else {
-        mailcheckMessage.innerText = "電郵地址格式錯誤";
+  if (email && mailcheckMessage) {
+    mailcheckMessage.addEventListener("click", function () {
+      var emailSuggestion = document.querySelector(".email-suggestion").innerText;
+
+      if (emailSuggestion) {
+        email.value = emailSuggestion;
+        this.innerText = "";
       }
     });
-  });
+    email.addEventListener("blur", mailcheck());
+  } //
+
+
+  var enformButton = document.querySelector(".en__submit");
+
+  if (enformButton) {
+    enformButton.addEventListener("click", function (event) {
+      event.preventDefault();
+      var enform = document.querySelector("form.en__component");
+      var isValid = validationSchema.isValid({
+        email: email.value
+      });
+      isValid.then(function (valid) {
+        if (valid) {
+          mailcheckMessage.innerText = "";
+          enform.submit();
+        } else {
+          mailcheckMessage.innerText = "電郵地址格式錯誤";
+        }
+      });
+    });
+  } //
+
+
+  var whatsapp = document.querySelector(".btn-whatsapp");
+  var facebook = document.querySelector(".btn-facebook");
+  var instagram = document.querySelector(".btn-instagram");
+
+  var fbShare = function fbShare() {
+    var baseURL = "https://www.facebook.com/sharer/sharer.php";
+    var u = "https://act.greenpeace.org/page/56266/petition/1?utm_campaign=general&utm_source=facebook&utm_medium=social&utm_content=thankyou_page";
+    var t = (window.innerHeight - 436) / 2;
+    var l = (window.innerWidth - 626) / 2;
+    window.open(baseURL + "?u=" + encodeURIComponent(u), "_blank", "width=626,height=436,top=" + t + ",left=" + l);
+  };
+
+  var mainShare = function mainShare() {
+    // WEB SHARE API
+    if (navigator.share) {
+      // we can use web share!
+      navigator.share({
+        title: "",
+        text: "",
+        url: "https://act.gp/2T27O5N"
+      }).then(function () {
+        return console.log("Successfully shared");
+      }).catch(function (error) {
+        return console.log("Error sharing:", error);
+      });
+    } else {
+      fbShare();
+    }
+  };
 });
-},{"sanitize.css/sanitize.css":"../node_modules/sanitize.css/sanitize.css","./main.scss":"main.scss","jquery":"../node_modules/jquery/dist/jquery.js","mailcheck":"../node_modules/mailcheck/src/mailcheck.js","yup":"../node_modules/yup/es/index.js"}],"../node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"sanitize.css/sanitize.css":"../node_modules/sanitize.css/sanitize.css","./main.scss":"main.scss","jquery":"../node_modules/jquery/dist/jquery.js","smooth-scroll":"../node_modules/smooth-scroll/dist/smooth-scroll.polyfills.min.js","mailcheck":"../node_modules/mailcheck/src/mailcheck.js","yup":"../node_modules/yup/es/index.js"}],"../node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -21914,7 +21959,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55157" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50210" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
+"use strict";
 import "sanitize.css/sanitize.css";
 import "./main.scss";
-//
 import $ from "jquery";
+import SmoothScroll from "smooth-scroll";
 import Mailcheck from "mailcheck";
 import * as yup from "yup";
-//
 $(function() {
-  const init = function() {
+  var scroll = new SmoothScroll('a[href*="#"]');
+  //
+  const enformInit = function() {
     $(".en__field--emailAddress").append(
       `<span class="mailcheck-message"></span>`
     );
@@ -14,7 +16,7 @@ $(function() {
     $(".en__field--firstName").hide();
     $(".en__submit button").addClass("btn btn-block btn-round btn--download");
   };
-  init();
+  enformInit();
   //
   const email = document.querySelector('input[name="supporter.emailAddress"]');
   const mailcheckMessage = document.querySelector(".mailcheck-message");
@@ -45,34 +47,72 @@ $(function() {
       }
     });
   };
-  email.addEventListener("blur", mailcheck());
-  mailcheckMessage.addEventListener("click", function() {
-    const emailSuggestion = document.querySelector(".email-suggestion")
-      .innerText;
-    if (emailSuggestion) {
-      email.value = emailSuggestion;
-      this.innerText = "";
-    }
-  });
   const validationSchema = yup.object({
     email: yup
       .string()
       .email()
       .required()
   });
-  //
-  const enformButton = document.querySelector(".en__submit");
-  enformButton.addEventListener("click", function(event) {
-    event.preventDefault();
-    const enform = document.querySelector("form.en__component");
-    let isValid = validationSchema.isValid({ email: email.value });
-    isValid.then(valid => {
-      if (valid) {
-        mailcheckMessage.innerText = "";
-        enform.submit();
-      } else {
-        mailcheckMessage.innerText = "電郵地址格式錯誤";
+  if (email && mailcheckMessage) {
+    mailcheckMessage.addEventListener("click", function() {
+      const emailSuggestion = document.querySelector(".email-suggestion")
+        .innerText;
+      if (emailSuggestion) {
+        email.value = emailSuggestion;
+        this.innerText = "";
       }
     });
-  });
+    email.addEventListener("blur", mailcheck());
+  }
+  //
+  const enformButton = document.querySelector(".en__submit");
+  if (enformButton) {
+    enformButton.addEventListener("click", function(event) {
+      event.preventDefault();
+      const enform = document.querySelector("form.en__component");
+      let isValid = validationSchema.isValid({
+        email: email.value
+      });
+      isValid.then(valid => {
+        if (valid) {
+          mailcheckMessage.innerText = "";
+          enform.submit();
+        } else {
+          mailcheckMessage.innerText = "電郵地址格式錯誤";
+        }
+      });
+    });
+  }
+  //
+  const whatsapp = document.querySelector(".btn-whatsapp");
+  const facebook = document.querySelector(".btn-facebook");
+  const instagram = document.querySelector(".btn-instagram");
+  const fbShare = () => {
+    var baseURL = "https://www.facebook.com/sharer/sharer.php";
+    var u =
+      "https://act.greenpeace.org/page/56266/petition/1?utm_campaign=general&utm_source=facebook&utm_medium=social&utm_content=thankyou_page";
+    var t = (window.innerHeight - 436) / 2;
+    var l = (window.innerWidth - 626) / 2;
+    window.open(
+      baseURL + "?u=" + encodeURIComponent(u),
+      "_blank",
+      "width=626,height=436,top=" + t + ",left=" + l
+    );
+  };
+  const mainShare = () => {
+    // WEB SHARE API
+    if (navigator.share) {
+      // we can use web share!
+      navigator
+        .share({
+          title: "",
+          text: "",
+          url: "https://act.gp/2T27O5N"
+        })
+        .then(() => console.log("Successfully shared"))
+        .catch(error => console.log("Error sharing:", error));
+    } else {
+      fbShare();
+    }
+  };
 });
